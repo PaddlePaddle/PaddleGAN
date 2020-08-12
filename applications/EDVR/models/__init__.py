8c46c4439b742f5dba8e182d50b6407c580954c3
lijianshe02 <jslict09@gmail.com>
2020-08-12 06:22:48 +0000

    delete inference irralated code

--- applications/EDVR/models/__init__.py
+++ applications/EDVR/models/__init__.py
@@ -1,31 +1,4 @@
 from .model import regist_model, get_model
-#from .attention_cluster import AttentionCluster
-#from .attention_lstm import AttentionLSTM
-#from .nextvlad import NEXTVLAD
-#from .nonlocal_model import NonLocal
-#from .tsm import TSM
-#from .tsn import TSN
-#from .stnet import STNET
-#from .ctcn import CTCN
-#from .bmn import BMN
-#from .bsn import BsnTem
-#from .bsn import BsnPem
-#from .ets import ETS
-#from .tall import TALL
 from .edvr import EDVR
 
-# regist models, sort by alphabet
-#regist_model("AttentionCluster", AttentionCluster)
-#regist_model("AttentionLSTM", AttentionLSTM)
-#regist_model("NEXTVLAD", NEXTVLAD)
-#regist_model('NONLOCAL', NonLocal)
-#regist_model("TSM", TSM)
-#regist_model("TSN", TSN)
-#regist_model("STNET", STNET)
-#regist_model("CTCN", CTCN)
-#regist_model("BMN", BMN)
-#regist_model("BsnTem", BsnTem)
-#regist_model("BsnPem", BsnPem)
-#regist_model("ETS", ETS)
-#regist_model("TALL", TALL)
 regist_model("EDVR", EDVR)
--- applications/EDVR/run.sh
+++ applications/EDVR/run.sh
@@ -11,73 +11,24 @@ mode=$1
 name=$2
 configs=$3
 
-pretrain="./tmp/name_map/paddle_state_dict.npz" # set pretrain model path if needed
-resume="" # set pretrain model path if needed
-save_dir="./data/checkpoints"
+#pretrain="./tmp/name_map/paddle_state_dict.npz" # set pretrain model path if needed
+#resume="" # set pretrain model path if needed
+#save_dir="./data/checkpoints"
 save_inference_dir="./data/inference_model"
 use_gpu=True
 fix_random_seed=False
 log_interval=1
 valid_interval=1
 
-#weights="./data/checkpoints/EDVR_epoch721.pdparams" #set the path of weights to enable eval and predicut, just ignore this when training
-
-#weights="./data/checkpoints_with_tsa/EDVR_epoch821.pdparams"
 weights="./weights/paddle_state_dict_L.npz"
-#weights="./weights/"
 
 
-#export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 export CUDA_VISIBLE_DEVICES=4,5,6,7   #0,1,5,6 fast,  2,3,4,7 slow
-#export CUDA_VISIBLE_DEVICES=7
 export FLAGS_fast_eager_deletion_mode=1
 export FLAGS_eager_delete_tensor_gb=0.0
 export FLAGS_fraction_of_gpu_memory_to_use=0.98
 
-if [ "$mode" == "train" ]; then
-    echo $mode $name $configs $resume $pretrain
-    if [ "$resume"x != ""x ]; then
-        python train.py --model_name=$name \
-                        --config=$configs \
-                        --resume=$resume \
-                        --log_interval=$log_interval \
-                        --valid_interval=$valid_interval \
-                        --use_gpu=$use_gpu \
-                        --save_dir=$save_dir \
-                        --fix_random_seed=$fix_random_seed
-    elif [ "$pretrain"x != ""x ]; then
-        python train.py --model_name=$name \
-                        --config=$configs \
-                        --pretrain=$pretrain \
-                        --log_interval=$log_interval \
-                        --valid_interval=$valid_interval \
-                        --use_gpu=$use_gpu \
-                        --save_dir=$save_dir \
-                        --fix_random_seed=$fix_random_seed
-    else
-        python train.py --model_name=$name \
-                        --config=$configs \
-                        --log_interval=$log_interval \
-                        --valid_interval=$valid_interval \
-                        --use_gpu=$use_gpu \
-                        --save_dir=$save_dir \
-                        --fix_random_seed=$fix_random_seed
-    fi
-elif [ "$mode"x == "eval"x ]; then
-    echo $mode $name $configs $weights
-    if [ "$weights"x != ""x ]; then
-        python eval.py --model_name=$name \
-                       --config=$configs \
-                       --log_interval=$log_interval \
-                       --weights=$weights \
-                       --use_gpu=$use_gpu
-    else
-        python eval.py --model_name=$name \
-                       --config=$configs \
-                       --log_interval=$log_interval \
-                       --use_gpu=$use_gpu
-    fi
-elif [ "$mode"x == "predict"x ]; then
+if [ "$mode"x == "predict"x ]; then
     echo $mode $name $configs $weights
     if [ "$weights"x != ""x ]; then
         python predict.py --model_name=$name \
