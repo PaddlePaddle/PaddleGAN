0970de7c95652f689a8ca0053d1b6ca953721795
LielinJiang <jianglielin@baidu.com>
2020-09-04 06:29:49 +0000

    fix save error

--- ppgan/utils/filesystem.py
+++ ppgan/utils/filesystem.py
@@ -3,17 +3,19 @@ import six
 import pickle
 import paddle
 
+
 def makedirs(dir):
     if not os.path.exists(dir):
         os.makedirs(dir)
 
-def save(state_dicts, file_name):
 
+def save(state_dicts, file_name):
     def convert(state_dict):
         model_dict = {}
-        
+
         for k, v in state_dict.items():
-            if isinstance(v, (paddle.framework.Variable, paddle.imperative.core.VarBase)):
+            if isinstance(
+                    v, (paddle.framework.Variable, paddle.fluid.core.VarBase)):
                 model_dict[k] = v.numpy()
             else:
                 model_dict[k] = v
@@ -22,14 +24,15 @@ def save(state_dicts, file_name):
 
     final_dict = {}
     for k, v in state_dicts.items():
-        if isinstance(v, (paddle.framework.Variable, paddle.imperative.core.VarBase)):
+        if isinstance(v,
+                      (paddle.framework.Variable, paddle.fluid.core.VarBase)):
             final_dict = convert(state_dicts)
             break
         elif isinstance(v, dict):
             final_dict[k] = convert(v)
         else:
             final_dict[k] = v
-    
+
     with open(file_name, 'wb') as f:
         pickle.dump(final_dict, f, protocol=2)
 
@@ -39,7 +42,3 @@ def load(file_name):
         state_dicts = pickle.load(f) if six.PY2 else pickle.load(
             f, encoding='latin1')
     return state_dicts
-
-    
-
-    
