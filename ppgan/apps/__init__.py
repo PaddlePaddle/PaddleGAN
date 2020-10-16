1f7e350c9d33bb07cb31585e5514be2ed2cb73d4
LielinJiang <jianglielin@baidu.com>
2020-10-16 11:33:28 +0000

    clean code

--- ppgan/apps/__init__.py
+++ ppgan/apps/__init__.py
@@ -3,3 +3,4 @@ from .deepremaster_predictor import DeepRemasterPredictor
 from .deoldify_predictor import DeOldifyPredictor
 from .realsr_predictor import RealSRPredictor
 from .edvr_predictor import EDVRPredictor
+from .first_order_predictor import FirstOrderPredictor
