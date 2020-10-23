f3a166a0868ae654e7aa184e897c304d812f5711
Liu Yiqun <liuyiqun01@baidu.com>
2020-10-23 08:05:17 +0000

    Add ips to the benchmark information.

--- ppgan/utils/timer.py
+++ ppgan/utils/timer.py
@@ -22,12 +22,20 @@ class TimeAverager(object):
     def reset(self):
         self._cnt = 0
         self._total_time = 0
+        self._total_samples = 0
 
-    def record(self, usetime):
+    def record(self, usetime, num_samples=None):
         self._cnt += 1
         self._total_time += usetime
+        if num_samples:
+            self._total_samples += num_samples
 
     def get_average(self):
         if self._cnt == 0:
             return 0
-        return self._total_time / self._cnt
+        return self._total_time / float(self._cnt)
+
+    def get_ips_average(self):
+        if not self._total_samples or self._cnt == 0:
+            return 0
+        return float(self._total_samples) / self._total_time
