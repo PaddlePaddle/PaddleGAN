9020cf3ce5c0dcb3497df77ee50c0e0541e094d2
qingqing01 <dangqingqing@baidu.com>
2020-10-29 14:47:56 +0800

    Add Copyright (#60)

--- ppgan/utils/registry.py
+++ ppgan/utils/registry.py
@@ -1,3 +1,18 @@
+#   Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserve.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 class Registry(object):
     """
     The registry that provides name -> object mapping, to support third-party users' custom modules.
@@ -13,7 +28,6 @@ class Registry(object):
     .. code-block:: python
         BACKBONE_REGISTRY.register(MyBackbone)
     """
-
     def __init__(self, name):
         """
         Args:
@@ -26,7 +40,8 @@ class Registry(object):
     def _do_register(self, name, obj):
         assert (
             name not in self._obj_map
-        ), "An object named '{}' was already registered in '{}' registry!".format(name, self._name)
+        ), "An object named '{}' was already registered in '{}' registry!".format(
+            name, self._name)
         self._obj_map[name] = obj
 
     def register(self, obj=None, name=None):
@@ -52,6 +67,8 @@ class Registry(object):
     def get(self, name):
         ret = self._obj_map.get(name)
         if ret is None:
-            raise KeyError("No object named '{}' found in '{}' registry!".format(name, self._name))
+            raise KeyError(
+                "No object named '{}' found in '{}' registry!".format(
+                    name, self._name))
 
         return ret
