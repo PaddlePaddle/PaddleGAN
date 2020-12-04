8ff47a507be69c2629017370e75faf0ee77bbbf3
LielinJiang <50691816+LielinJiang@users.noreply.github.com>
2020-12-04 19:54:22 +0800

    fix type and lazy import dlib (#109)

--- ppgan/faceutils/dlibutils/dlib_utils.py
+++ ppgan/faceutils/dlibutils/dlib_utils.py
@@ -17,7 +17,7 @@ import os.path as osp
 
 import numpy as np
 from PIL import Image
-import dlib
+from paddle.utils import try_import
 import cv2
 from ..image import resize_by_max
 from paddle.utils.download import get_weights_path_from_url
@@ -26,6 +26,7 @@ LANDMARKS_WEIGHT_URL = 'https://paddlegan.bj.bcebos.com/models/lms.dat'
 
 
 def detect(image: Image):
+    dlib = try_import('dlib')
     image = np.asarray(image)
     h, w = image.shape[:2]
     image = resize_by_max(image, 361)
@@ -43,6 +44,7 @@ def detect(image: Image):
 
 
 def crop(image: Image, face, up_ratio, down_ratio, width_ratio):
+    dlib = try_import('dlib')
     width, height = image.size
     face_height = face.height()
     face_width = face.width()
@@ -96,6 +98,7 @@ def crop(image: Image, face, up_ratio, down_ratio, width_ratio):
 
 
 def crop_by_image_size(image: Image, face):
+    dlib = try_import('dlib')
     center = face.center()
     width, height = image.size
     if width > height:
@@ -123,6 +126,7 @@ def crop_by_image_size(image: Image, face):
 
 
 def landmarks(image: Image, face):
+    dlib = try_import('dlib')
     weight_path = get_weights_path_from_url(LANDMARKS_WEIGHT_URL)
     predictor = dlib.shape_predictor(weight_path)
     shape = predictor(np.asarray(image), face).parts()
@@ -130,6 +134,7 @@ def landmarks(image: Image, face):
 
 
 def crop_from_array(image: np.array, face):
+    dlib = try_import('dlib')
     ratio = 0.20 / 0.85  # delta_size / face_size
     height, width = image.shape[:2]
     face_height = face.height()
