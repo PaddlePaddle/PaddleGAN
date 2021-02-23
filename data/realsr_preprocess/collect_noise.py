9f36b055ef0833361cc3b8ae723b83b86bad3f7a
LielinJiang <50691816+LielinJiang@users.noreply.github.com>
2021-02-23 10:25:52 +0800

    [Cherrypick-2.0.0-beta] add dped docs (#172)
    * add dped docs

--- data/realsr_preprocess/collect_noise.py
+++ data/realsr_preprocess/collect_noise.py
@@ -29,7 +29,7 @@ opt = parser.parse_args()
 # define input and target directories
 cur_path = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(cur_path, './paths.yml'), 'r') as stream:
-    PATHS = yaml.load(stream)
+    PATHS = yaml.load(stream, Loader=yaml.SafeLoader)
 
 
 def noise_patch(rgb_img, sp, max_var, min_mean):
