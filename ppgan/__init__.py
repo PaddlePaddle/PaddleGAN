517f04e3f3197a64bc9232bb540ce4d0f141baae
LielinJiang <jianglielin@baidu.com>
2021-02-26 12:33:36 +0000

    fix init

--- ppgan/__init__.py
+++ ppgan/__init__.py
@@ -11,13 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-from . import apps
-from . import datasets
-from . import engine
-from . import faceutils
-from . import metrics
-from . import models
-from . import modules
-from . import solver
-from . import utils
