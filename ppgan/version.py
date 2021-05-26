b736a72175e826d1da90a1858f408e1a3b69a599
LielinJiang <jianglielin@baidu.com>
2021-05-26 06:43:02 +0000

    update version

--- ppgan/version.py
+++ ppgan/version.py
@@ -1,4 +1,4 @@
-#   Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserve.
+#   Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserve.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .version import ppgan_version as __version__
+ppgan_version = '2.0.0'
