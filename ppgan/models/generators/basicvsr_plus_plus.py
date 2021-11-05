3e71e6a0d4f9f93de62a6c02fdd1bf05e763bff2
wangna11BD <wangna11@baidu.com>
2021-11-05 05:07:21 +0000

    add copy right and docs

--- ppgan/models/generators/basicvsr_plus_plus.py
+++ ppgan/models/generators/basicvsr_plus_plus.py
@@ -1,4 +1,16 @@
-# Copyright (c) MMEditing Authors.
+#   Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserve.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import numpy as np
 
@@ -19,6 +31,11 @@ class BasicVSRPlusPlus(nn.Layer):
     Paper:
         BasicVSR++: Improving Video Super-Resolution with Enhanced Propagation
         and Alignment
+
+    Adapted from 'https://github.com/open-mmlab/mmediting'
+    'mmediting/blob/master/mmedit/models/backbones/sr_backbones/basicvsr_pp.py'
+    Copyright (c) MMEditing Authors.
+
     Args:
         mid_channels (int, optional): Channel number of the intermediate
             features. Default: 64.
