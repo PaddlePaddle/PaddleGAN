6a31877fef9b954c90136b51a5938114d565dba7
hysunflower <52739577+hysunflower@users.noreply.github.com>
2021-12-02 13:57:29 +0800

    update_scripts (#505)

--- benchmark/run_benchmark.sh
+++ benchmark/run_benchmark.sh
@@ -8,7 +8,7 @@ function _set_params(){
     fp_item=${3:-"fp32"}        # fp32|fp16
     mode=${4:-"epochs"}
     max_iter=${5:-"500"}       # 可选，如果需要修改代码提前中断
-    model_name=${6:-"model_name"}
+    model_item=${6:-"model_item"}
     config=${7:-"config"}
     log_interval=${8:-"1"}
     run_log_path=${TRAIN_LOG_DIR:-$(pwd)}  # TRAIN_LOG_DIR 后续QA设置该参数
@@ -22,13 +22,13 @@ function _set_params(){
     keyword_loss="G_idt_A_loss:"
     skip_steps=5
     ips_unit="images/s"
-
+    model_name=${model_item}_bs${batch_size}_${fp_item}
 #   以下不用修改
     device=${CUDA_VISIBLE_DEVICES//,/ }
     arr=(${device})
     num_gpu_devices=${#arr[*]}
-    log_file=${run_log_path}/${model_name}_${run_mode}_bs${batch_size}_${fp_item}_${num_gpu_devices}
-    res_log_file=${run_log_path}/${model_name}_${run_mode}_bs${batch_size}_${fp_item}_${num_gpu_devices}_speed
+    log_file=${run_log_path}/${model_item}_${run_mode}_bs${batch_size}_${fp_item}_${num_gpu_devices}
+    res_log_file=${run_log_path}/${model_item}_${run_mode}_bs${batch_size}_${fp_item}_${num_gpu_devices}_speed
     log_profile=${run_log_path}/${model_name}_model.profile
 }
 
