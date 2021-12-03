bede80d8aa0fd70493990f92e7188c81031b2acd
LielinJiang <jianglielin@baidu.com>
2021-12-03 06:12:06 +0000

    add recurrent vsr predictor

--- ppgan/apps/__init__.py
+++ ppgan/apps/__init__.py
@@ -31,3 +31,4 @@ from .wav2lip_predictor import Wav2LipPredictor
 from .mpr_predictor import MPRPredictor
 from .lapstyle_predictor import LapStylePredictor
 from .photopen_predictor import PhotoPenPredictor
+from .recurrent_vsr_predictor import PPMSVSRPredictor, BasicVSRPredictor, BasiVSRPlusPlusPredictor, IconVSRPredictor, PPMSVSRLargePredictor
