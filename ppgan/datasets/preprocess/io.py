424ab9eb5db031d178f6048b2f13aa0472ecfaab
wangna11BD <79366697+wangna11BD@users.noreply.github.com>
2021-12-15 20:07:39 +0800

    fix reds dataset (#527)
    * fix reds dataset
    
    * fix CI

--- ppgan/datasets/preprocess/io.py
+++ ppgan/datasets/preprocess/io.py
@@ -179,3 +179,151 @@ class GetNeighboringFramesIdx:
         datas['interval'] = interval
 
         return datas
+
+
+@PREPROCESS.register()
+class GetFrameIdx:
+    """Generate frame index for REDS datasets.
+
+    Args:
+        interval_list (list[int]): Interval list for temporal augmentation.
+            It will randomly pick an interval from interval_list and sample
+            frame index with the interval.
+        frames_per_clip(int): Number of frames per clips. Default: 99 for
+            REDS dataset.
+    """
+    def __init__(self, interval_list, frames_per_clip=99):
+        self.interval_list = interval_list
+        self.frames_per_clip = frames_per_clip
+
+    def __call__(self, results):
+        """Call function.
+
+        Args:
+            results (dict): A dict containing the necessary information and
+                data for augmentation.
+
+        Returns:
+            dict: A dict containing the processed data and information.
+        """
+        clip_name, frame_name = results['key'].split('/')
+        center_frame_idx = int(frame_name)
+        num_half_frames = results['num_frames'] // 2
+
+        interval = np.random.choice(self.interval_list)
+        # ensure not exceeding the borders
+        start_frame_idx = center_frame_idx - num_half_frames * interval
+        end_frame_idx = center_frame_idx + num_half_frames * interval
+        while (start_frame_idx < 0) or (end_frame_idx > self.frames_per_clip):
+            center_frame_idx = np.random.randint(0, self.frames_per_clip + 1)
+            start_frame_idx = center_frame_idx - num_half_frames * interval
+            end_frame_idx = center_frame_idx + num_half_frames * interval
+        frame_name = f'{center_frame_idx:08d}'
+        neighbor_list = list(
+            range(center_frame_idx - num_half_frames * interval,
+                  center_frame_idx + num_half_frames * interval + 1, interval))
+
+        lq_path_root = results['lq_path']
+        gt_path_root = results['gt_path']
+        lq_path = [
+            os.path.join(lq_path_root, clip_name, f'{v:08d}.png')
+            for v in neighbor_list
+        ]
+        gt_path = [os.path.join(gt_path_root, clip_name, f'{frame_name}.png')]
+        results['lq_path'] = lq_path
+        results['gt_path'] = gt_path
+        results['interval'] = interval
+
+        return results
+
+    def __repr__(self):
+        repr_str = self.__class__.__name__
+        repr_str += (f'(interval_list={self.interval_list}, '
+                     f'frames_per_clip={self.frames_per_clip})')
+        return repr_str
+
+
+@PREPROCESS.register()
+class GetFrameIdxwithPadding:
+    """Generate frame index with padding for REDS dataset and Vid4 dataset
+    during testing.
+
+    Args:
+         padding (str): padding mode, one of
+            'replicate' | 'reflection' | 'reflection_circle' | 'circle'.
+
+            Examples: current_idx = 0, num_frames = 5
+            The generated frame indices under different padding mode:
+
+                replicate: [0, 0, 0, 1, 2]
+                reflection: [2, 1, 0, 1, 2]
+                reflection_circle: [4, 3, 0, 1, 2]
+                circle: [3, 4, 0, 1, 2]
+
+        filename_tmpl (str): Template for file name. Default: '{:08d}'.
+    """
+    def __init__(self, padding, filename_tmpl='{:08d}'):
+        if padding not in ('replicate', 'reflection', 'reflection_circle',
+                           'circle'):
+            raise ValueError(f'Wrong padding mode {padding}.'
+                             'Should be "replicate", "reflection", '
+                             '"reflection_circle",  "circle"')
+        self.padding = padding
+        self.filename_tmpl = filename_tmpl
+
+    def __call__(self, results):
+        """Call function.
+
+        Args:
+            results (dict): A dict containing the necessary information and
+                data for augmentation.
+
+        Returns:
+            dict: A dict containing the processed data and information.
+        """
+        clip_name, frame_name = results['key'].split('/')
+        current_idx = int(frame_name)
+        max_frame_num = results['max_frame_num'] - 1  # start from 0
+        num_frames = results['num_frames']
+        num_pad = num_frames // 2
+
+        frame_list = []
+        for i in range(current_idx - num_pad, current_idx + num_pad + 1):
+            if i < 0:
+                if self.padding == 'replicate':
+                    pad_idx = 0
+                elif self.padding == 'reflection':
+                    pad_idx = -i
+                elif self.padding == 'reflection_circle':
+                    pad_idx = current_idx + num_pad - i
+                else:
+                    pad_idx = num_frames + i
+            elif i > max_frame_num:
+                if self.padding == 'replicate':
+                    pad_idx = max_frame_num
+                elif self.padding == 'reflection':
+                    pad_idx = max_frame_num * 2 - i
+                elif self.padding == 'reflection_circle':
+                    pad_idx = (current_idx - num_pad) - (i - max_frame_num)
+                else:
+                    pad_idx = i - num_frames
+            else:
+                pad_idx = i
+            frame_list.append(pad_idx)
+
+        lq_path_root = results['lq_path']
+        gt_path_root = results['gt_path']
+        lq_paths = [
+            os.path.join(lq_path_root, clip_name,
+                         f'{self.filename_tmpl.format(idx)}.png')
+            for idx in frame_list
+        ]
+        gt_paths = [os.path.join(gt_path_root, clip_name, f'{frame_name}.png')]
+        results['lq_path'] = lq_paths
+        results['gt_path'] = gt_paths
+
+        return results
+
+    def __repr__(self):
+        repr_str = self.__class__.__name__ + f"(padding='{self.padding}')"
+        return repr_str
