2caad194029286ce3dbc445430e853d4894c64f0
wangna11BD <wangna11@baidu.com>
2021-12-21 03:50:24 +0000

    fix copyright

--- applications/tools/wav2lip.py
+++ applications/tools/wav2lip.py
@@ -1,3 +1,17 @@
+#  Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserve.
+#
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+
 import argparse
 
 import paddle
@@ -103,11 +117,10 @@ parser.add_argument(
     type=str,
     default='sfd',
     help="face detector to be used, can choose s3fd or blazeface")
-parser.add_argument(
-    "--face_enhancement",
-    dest="face_enhancement",
-    action="store_true",
-    help="use face enhance for face")
+parser.add_argument("--face_enhancement",
+                    dest="face_enhancement",
+                    action="store_true",
+                    help="use face enhance for face")
 parser.set_defaults(face_enhancement=False)
 
 if __name__ == "__main__":
@@ -115,17 +128,17 @@ if __name__ == "__main__":
     if args.cpu:
         paddle.set_device('cpu')
 
-    predictor = Wav2LipPredictor(checkpoint_path = args.checkpoint_path,
-                                 static = args.static,
-                                 fps = args.fps,
-                                 pads = args.pads,
-                                 face_det_batch_size = args.face_det_batch_size,
-                                 wav2lip_batch_size = args.wav2lip_batch_size,
-                                 resize_factor = args.resize_factor,
-                                 crop = args.crop,
-                                 box = args.box,
-                                 rotate = args.rotate,
-                                 nosmooth = args.nosmooth,
-                                 face_detector = args.face_detector,
-                                 face_enhancement = args.face_enhancement)
+    predictor = Wav2LipPredictor(checkpoint_path=args.checkpoint_path,
+                                 static=args.static,
+                                 fps=args.fps,
+                                 pads=args.pads,
+                                 face_det_batch_size=args.face_det_batch_size,
+                                 wav2lip_batch_size=args.wav2lip_batch_size,
+                                 resize_factor=args.resize_factor,
+                                 crop=args.crop,
+                                 box=args.box,
+                                 rotate=args.rotate,
+                                 nosmooth=args.nosmooth,
+                                 face_detector=args.face_detector,
+                                 face_enhancement=args.face_enhancement)
     predictor.run(args.face, args.audio, args.outfile)
