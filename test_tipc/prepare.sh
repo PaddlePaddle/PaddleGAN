dce1b4f856799238b6fa7a2979450ee4afcd3b44
lzzyzlbb <287246233@qq.com>
2022-02-10 13:34:37 +0000

    update benchmark

--- test_tipc/prepare.sh
+++ test_tipc/prepare.sh
@@ -40,32 +40,33 @@ fi
 #                 'whole_infer
 
 if [ ${MODE} = "lite_train_lite_infer" ];then
-    if [ ${model_name} == "pix2pix" ]; then
+    
+    case ${model_name} in
+    Pix2pix)
         rm -rf ./data/pix2pix*
         wget -nc -P ./data/ https://paddlegan.bj.bcebos.com/datasets/pix2pix_facade_lite.tar --no-check-certificate
-        cd ./data/ && tar xf pix2pix_facade_lite.tar && cd ../
-    elif [ ${model_name} == "cyclegan" ]; then
+        cd ./data/ && tar xf pix2pix_facade_lite.tar && cd ../ ;;
+    CycleGAN)
         rm -rf ./data/cyclegan*
         wget -nc -P ./data/ https://paddlegan.bj.bcebos.com/datasets/cyclegan_horse2zebra_lite.tar --no-check-certificate
-        cd ./data/ && tar xf cyclegan_horse2zebra_lite.tar && cd ../
-    elif [ ${model_name} == "stylegan2" ]; then
+        cd ./data/ && tar xf cyclegan_horse2zebra_lite.tar && cd ../ ;;
+    StyleGANv2)
         rm -rf ./data/ffhq*
         wget -nc -P ./data/ https://paddlegan.bj.bcebos.com/datasets/ffhq.tar --no-check-certificate
-        cd ./data/ && tar xf ffhq.tar && cd ../
-    elif [ ${model_name} == "fom" ]; then
+        cd ./data/ && tar xf ffhq.tar && cd ../ ;;
+    FOMM)
         rm -rf ./data/fom_lite*
         wget -nc -P ./data/ https://paddlegan.bj.bcebos.com/datasets/fom_lite.tar  --no-check-certificate --no-check-certificate
-        cd ./data/ && tar xf fom_lite.tar && cd ../
-    elif [ ${model_name} == "basicvsr" ]; then
+        cd ./data/ && tar xf fom_lite.tar && cd ../ ;;
+    edvr|basicvsr|msvsr)
         rm -rf ./data/reds*
         wget -nc -P ./data/ https://paddlegan.bj.bcebos.com/datasets/reds_lite.tar --no-check-certificate
-        cd ./data/ && tar xf reds_lite.tar && cd ../
-    elif [ ${model_name} == "msvsr" ]; then
-        rm -rf ./data/reds*
-        wget -nc -P ./data/ https://paddlegan.bj.bcebos.com/datasets/reds_lite.tar --no-check-certificate
-        cd ./data/ && tar xf reds_lite.tar && cd ../
-    fi
-    
+        cd ./data/ && tar xf reds_lite.tar && cd ../ ;;
+    esrgan)
+        rm -rf ./data/DIV2K*
+        wget -nc -P ./data/ https://paddlegan.bj.bcebos.com/datasets/DIV2KandSet14.tar --no-check-certificate
+        cd ./data/ && tar xf DIV2KandSet14.tar && cd ../ ;;
+    esac   
 elif [ ${MODE} = "whole_train_whole_infer" ];then
     if [ ${model_name} == "pix2pix" ]; then
         rm -rf ./data/facades*
