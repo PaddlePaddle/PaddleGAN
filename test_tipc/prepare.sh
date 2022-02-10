e1982a3ade93fba89461f129be2fb4b5a76dcc3e
BrilliantYuKaimin <91609464+BrilliantYuKaimin@users.noreply.github.com>
2022-02-10 15:30:35 +0800

    modify urls for singan in prepare.sh

--- test_tipc/prepare.sh
+++ test_tipc/prepare.sh
@@ -62,8 +62,8 @@ if [ ${MODE} = "lite_train_lite_infer" ];then
         cd ./data/ && tar xf reds_lite.tar && cd ../
     elif [ ${model_name} == "singan" ]; then
         rm -rf ./data/SinGAN*
-        wget -nc https://bj.bcebos.com/v1/ai-studio-online/032eecbb73cf4b948b4848aac68d0f3422190aa2da7a456aaf8ad4c5811c614b -O ./data/SinGAN-official_images.zip --no-check-certificate
-        cd ./data/ && unzip -q SinGAN-official_images.zip && cd ../
+        wget -nc -P ./data/ https://paddlegan.bj.bcebos.com/datasets/singan-official_images.zip --no-check-certificate
+        cd ./data/ && unzip -q singan-official_images.zip && cd ../
         mkdir -p ./data/singan
         mv ./data/SinGAN-official_images/Images/stone.png ./data/singan
     fi
@@ -79,8 +79,8 @@ elif [ ${MODE} = "whole_train_whole_infer" ];then
         cd ./data/ && unzip horse2zebra.zip && cd ../
     elif [ ${model_name} == "singan" ]; then
         rm -rf ./data/SinGAN*
-        wget -nc https://bj.bcebos.com/v1/ai-studio-online/032eecbb73cf4b948b4848aac68d0f3422190aa2da7a456aaf8ad4c5811c614b -O ./data/SinGAN-official_images.zip --no-check-certificate
-        cd ./data/ && unzip -q SinGAN-official_images.zip && cd ../
+        wget -nc -P ./data/ https://paddlegan.bj.bcebos.com/datasets/singan-official_images.zip --no-check-certificate
+        cd ./data/ && unzip -q singan-official_images.zip && cd ../
         mkdir -p ./data/singan
         mv ./data/SinGAN-official_images/Images/stone.png ./data/singan
     fi
@@ -111,8 +111,8 @@ elif [ ${MODE} = "lite_train_whole_infer" ];then
         cd ./data/ && tar xf reds_lite.tar && cd ../
     elif [ ${model_name} == "singan" ]; then
         rm -rf ./data/SinGAN*
-        wget -nc https://bj.bcebos.com/v1/ai-studio-online/032eecbb73cf4b948b4848aac68d0f3422190aa2da7a456aaf8ad4c5811c614b -O ./data/SinGAN-official_images.zip --no-check-certificate
-        cd ./data/ && unzip -q SinGAN-official_images.zip && cd ../
+        wget -nc -P ./data/ https://paddlegan.bj.bcebos.com/datasets/singan-official_images.zip --no-check-certificate
+        cd ./data/ && unzip -q singan-official_images.zip && cd ../
         mkdir -p ./data/singan
         mv ./data/SinGAN-official_images/Images/stone.png ./data/singan
     fi
@@ -160,13 +160,10 @@ elif [ ${MODE} = "whole_infer" ];then
         cd ./data/ && tar xf reds_lite.tar && cd ../
     elif [ ${model_name} == "singan" ]; then
         rm -rf ./data/SinGAN*
-        wget -nc https://bj.bcebos.com/v1/ai-studio-online/032eecbb73cf4b948b4848aac68d0f3422190aa2da7a456aaf8ad4c5811c614b -O ./data/SinGAN-official_images.zip --no-check-certificate
-        if [ ! -d inference ]; then
-            mkdir inference
-        fi
-        wget -nc https://bj.bcebos.com/v1/ai-studio-online/c3a9b05e9c1b4b3dab21b47f0ff2aa4155f9ea42e9c04010a235b257b66564e0 -O ./inference/singan_static.zip --no-check-certificate
-        cd ./data/ && unzip -q SinGAN-official_images.zip && cd ../
-        cd ./inference/ && unzip -q singan_static.zip && cd ../
+        wget -nc -P ./data/ https://paddlegan.bj.bcebos.com/datasets/singan-official_images.zip --no-check-certificate
+        wget -nc -P ./inference https://paddlegan.bj.bcebos.com/datasets/singan.zip --no-check-certificate
+        cd ./data/ && unzip -q singan-official_images.zip && cd ../
+        cd ./inference/ && unzip -q singan.zip && cd ../
         mkdir -p ./data/singan
         mv ./data/SinGAN-official_images/Images/stone.png ./data/singan
     fi
