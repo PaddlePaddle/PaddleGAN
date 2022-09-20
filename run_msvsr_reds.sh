9153eed0fae2115eba01741537208a8131475988
niuliling123 <sdust_niull@163.com>
2022-09-20 15:14:44 +0800

    update

--- run_msvsr_reds.sh
+++ run_msvsr_reds.sh
@@ -1,3 +1,5 @@
 #export GLOG_v=6
-python -u tools/main.py --config-file configs/msvsr_reds.yaml --amp --amp_level O1
+python -m paddle.distributed.launch --gpus=0,1,2,3 tools/main.py --config-file configs/msvsr_reds.yaml --amp --amp_level O1
+
+#python -u tools/main.py --config-file configs/msvsr_reds.yaml --amp --amp_level O1
 
