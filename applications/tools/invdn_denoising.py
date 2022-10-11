bcc8ef3a145112f24b198c3bd58fa46ac1f4d723
hnmizuho <yanyan1999421@gmail.com>
2022-10-11 18:16:44 +0800

    correct invdn_denoising

--- applications/tools/invdn_denoising.py
+++ applications/tools/invdn_denoising.py
@@ -1,23 +1,23 @@
-#  Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserve.
+# Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserve.
 #
-#Licensed under the Apache License, Version 2.0 (the "License");
-#you may not use this file except in compliance with the License.
-#You may obtain a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-#    http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
-#Unless required by applicable law or agreed to in writing, software
-#distributed under the License is distributed on an "AS IS" BASIS,
-#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#See the License for the specific language governing permissions and
-#limitations under the License.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import os
 import sys
 import argparse
 
 import paddle
-from ppgan.apps import SwinIRPredictor
+from ppgan.apps import InvDNPredictor
 
 sys.path.insert(0, os.getcwd())
 
@@ -54,7 +54,7 @@ if __name__ == "__main__":
     if args.cpu:
         paddle.set_device('cpu')
 
-    predictor = SwinIRPredictor(output_path=args.output_path,
-                                weight_path=args.weight_path,
-                                seed=args.seed)
+    predictor = InvDNPredictor(output_path=args.output_path,
+                               weight_path=args.weight_path,
+                               seed=args.seed)
     predictor.run(images_path=args.images_path)
