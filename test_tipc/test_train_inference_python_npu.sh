029edf8677512a66162620d02dc6635fa4466e31
duanyanhui <45005871+YanhuiDua@users.noreply.github.com>
2023-04-21 13:59:23 +0800

    update tipc scripts for time-consuming model on npu (#780)

--- test_tipc/test_train_inference_python_npu.sh
+++ test_tipc/test_train_inference_python_npu.sh
@@ -24,6 +24,16 @@ sed -i "s/--device:gpu/--device:npu/g" $FILENAME
 sed -i "s/--benchmark:True/--benchmark:False/g" $FILENAME
 dataline=`cat $FILENAME`
 
+# change total iters/epochs for npu/xpu to accelaration
+modelname=$(echo $FILENAME | cut -d '/' -f3)
+if  [ $modelname == "Pix2pix" ]; then
+    sed -i "s/lite_train_lite_infer=10/lite_train_lite_infer=1/g" $FILENAME
+fi
+
+if  [ $modelname == "edvr" ]; then
+    sed -i "s/lite_train_lite_infer=100/lite_train_lite_infer=10/g" $FILENAME
+fi
+
 # change gpu to npu in execution script
 sed -i 's/\"gpu\"/\"npu\"/g' test_tipc/test_train_inference_python.sh
 sed -i 's/--gpus/--npus/g' test_tipc/test_train_inference_python.sh
