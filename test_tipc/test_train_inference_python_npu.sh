cdb617d6f5329146a541e28273b23b51605029cd
duanyanhui <45005871+YanhuiDua@users.noreply.github.com>
2023-04-25 20:04:32 +0800

    Fix npu tipc to update snapshot_config (#782)
    * update tipc scripts for time-consuming model on npu

--- test_tipc/test_train_inference_python_npu.sh
+++ test_tipc/test_train_inference_python_npu.sh
@@ -24,23 +24,26 @@ sed -i "s/--device:gpu/--device:npu/g" $FILENAME
 sed -i "s/--benchmark:True/--benchmark:False/g" $FILENAME
 dataline=`cat $FILENAME`
 
+# parser params
+IFS=$'\n'
+lines=(${dataline})
+
 # change total iters/epochs for npu/xpu to accelaration
-modelname=$(echo $FILENAME | cut -d '/' -f3)
+modelname=$(func_parser_value "${lines[1]}")
+echo $modelname
 if  [ $modelname == "Pix2pix" ]; then
     sed -i "s/lite_train_lite_infer=10/lite_train_lite_infer=1/g" $FILENAME
+    sed -i "s/-o log_config.interval=1/-o log_config.interval=1 snapshot_config.interval=1/g" $FILENAME
 fi
 
 if  [ $modelname == "edvr" ]; then
     sed -i "s/lite_train_lite_infer=100/lite_train_lite_infer=10/g" $FILENAME
+    sed -i "s/snapshot_config.interval=25/snapshot_config.interval=5/g" $FILENAME
 fi
 
 # change gpu to npu in execution script
 sed -i 's/\"gpu\"/\"npu\"/g' test_tipc/test_train_inference_python.sh
 sed -i 's/--gpus/--npus/g' test_tipc/test_train_inference_python.sh
-
-# parser params
-IFS=$'\n'
-lines=(${dataline})
 
 # pass parameters to test_train_inference_python.sh
 cmd="bash test_tipc/test_train_inference_python.sh ${FILENAME} $2"
