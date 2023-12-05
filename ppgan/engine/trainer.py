18da30a76854bef13c5977ce67c086724de764ec
mmglove <guomengmeng01@baidu.com>
2023-12-05 13:10:31 +0800

    fix

--- ppgan/engine/trainer.py
+++ ppgan/engine/trainer.py
@@ -355,10 +355,11 @@ class Trainer:
             eta_str = str(datetime.timedelta(seconds=int(eta)))
             message += f'eta: {eta_str}'
 
-        max_mem_reserved_str = f" max_mem_reserved: {paddle.device.cuda.max_memory_reserved()} B"
-        max_mem_allocated_str = f" max_mem_allocated: {paddle.device.cuda.max_memory_allocated()} B"
-        message += max_mem_reserved_str
-        message += max_mem_allocated_str
+        if paddle.device.is_compiled_with_cuda():
+            max_mem_reserved_str = f" max_mem_reserved: {paddle.device.cuda.max_memory_reserved() // (1024 ** 2)} MB"
+            max_mem_allocated_str = f" max_mem_allocated: {paddle.device.cuda.max_memory_allocated() // (1024 ** 2)} MB"
+            message += max_mem_reserved_str
+            message += max_mem_allocated_str
         # print the message
         self.logger.info(message)
 
